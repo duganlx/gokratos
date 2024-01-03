4d3c3906c907dc5e994399b41c63e826e93fbac2
duganlx <840797783@qq.com>
2024-01-03 06:28:38 +0000

    test time.unix

--- utils/common_test.go
+++ utils/common_test.go
@@ -1,12 +1,20 @@
 package utils_test
 
 import (
+	"fmt"
 	"gokratos/utils"
 	"testing"
+	"time"
 )
 
 func Test_GetRandom(t *testing.T) {
 	secret := utils.GetRandom(64)
 
 	t.Logf("secret(length: %d): %v", len(secret), secret)
+}
+
+func TestTime(t *testing.T) {
+	dt := time.Unix(1704259198000/1000, 0)
+
+	fmt.Println(dt.Add(8 * time.Hour).Format("2006-01-02 15:04:05"))
 }
