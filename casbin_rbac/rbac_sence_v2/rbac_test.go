57cd2e3e2bc7008fb17a4967afb06c5afd411c2d
duganlx <840797783@qq.com>
2024-01-05 00:51:45 +0000

    test casbin for module

--- casbin_rbac/rbac_sence_v2/rbac_test.go
+++ casbin_rbac/rbac_sence_v2/rbac_test.go
@@ -72,4 +72,160 @@ func TestUser2Src(t *testing.T) {
 
 func TestUserGroup2Src(t *testing.T) {
 
+	e := NewEnforcer()
+
+	var ok bool
+	var err error
+
+	ok, err = e.Enforce("USER:lvx", "DRW011:v1:test", "*", "*")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW011:v1:test", "conf", "*")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW011:v1:test", "conf", "r")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW011:v1:test", "conf", "w")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+
+	ok, err = e.Enforce("USER:lvx", "DRW012:v1:test", "*", "*")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW012:v1:test", "conf", "*")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW012:v1:test", "conf", "r")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW012:v1:test", "conf", "w")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+
+	ok, err = e.Enforce("USER:lvx", "DRW013:v1:test", "*", "w")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW013:v1:test", "conf", "w")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW013:v1:test", "conf", "r")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+
+	ok, err = e.Enforce("USER:lvx", "DRW014:v1:test", "conf", "r")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW014:v1:test", "other", "r")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW014:v1:test", "conf", "w")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+}
+
+func TestSrcGroup2Src(t *testing.T) {
+	e := NewEnforcer()
+
+	var ok bool
+	var err error
+
+	ok, err = e.Enforce("USER:lvx", "DRW101:v1:test", "*", "*")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW101:v1:test", "conf", "*")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW101:v1:test", "conf", "r")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW101:v1:test", "conf", "w")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+
+	ok, err = e.Enforce("USER:lvx", "DRW102:v1:test", "*", "*")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW102:v1:test", "conf", "*")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW102:v1:test", "conf", "r")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW102:v1:test", "conf", "w")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+
+	ok, err = e.Enforce("USER:lvx", "DRW103:v1:test", "*", "w")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW103:v1:test", "conf", "w")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW103:v1:test", "conf", "r")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+
+	ok, err = e.Enforce("USER:lvx", "DRW104:v1:test", "conf", "r")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW104:v1:test", "other", "r")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "DRW104:v1:test", "conf", "w")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+}
+
+func TestUserGroup2SrcGroup(t *testing.T) {
+	e := NewEnforcer()
+
+	var ok bool
+	var err error
+
+	ok, err = e.Enforce("USER:lvx", "EAM101:v1:test", "*", "*")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "EAM101:v1:test", "conf", "*")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "EAM101:v1:test", "conf", "r")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "EAM101:v1:test", "conf", "w")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+
+	ok, err = e.Enforce("USER:lvx", "EAM102:v1:test", "*", "*")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "EAM102:v1:test", "conf", "*")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "EAM102:v1:test", "conf", "r")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "EAM102:v1:test", "conf", "w")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+
+	ok, err = e.Enforce("USER:lvx", "EAM103:v1:test", "*", "w")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "EAM103:v1:test", "conf", "w")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "EAM103:v1:test", "conf", "r")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+
+	ok, err = e.Enforce("USER:lvx", "EAM104:v1:test", "conf", "r")
+	assert.True(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "EAM104:v1:test", "other", "r")
+	assert.False(t, ok)
+	assert.Nil(t, err)
+	ok, err = e.Enforce("USER:lvx", "EAM104:v1:test", "conf", "w")
+	assert.False(t, ok)
+	assert.Nil(t, err)
 }
